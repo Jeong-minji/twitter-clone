56b31eff77c49f11d5a455fd19c20636583c84cd
Jeong-minji <alswl4152@naver.com>
2021-01-06 21:54:08 +0900

    Setup Firebase
    Setup Firebase

---
+++ ntwitter/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCz2_BjcSPD-WedS4CMmsBJvEyVcMR4UPI",
+    authDomain: "ntwitter-19a50.firebaseapp.com",
+    projectId: "ntwitter-19a50",
+    storageBucket: "ntwitter-19a50.appspot.com",
+    messagingSenderId: "440329250326",
+    appId: "1:440329250326:web:800bf8db5c5aca2cf0b737"
+};
+
+export default firebase.initializeApp(firebaseConfig);
